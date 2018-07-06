b814c0a8fd6ae93db12812dd648ed348722e8eae
Berry  Semexan <bsemexan@gmail.com>
2018-07-06 07:00:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,7 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id
       redirect '/users/home'
     end
+    redirect '/sessions/login'
   end
 
   get '/sessions/logout' do 
