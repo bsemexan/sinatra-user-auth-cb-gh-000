77cda5ecc9088157dc395a6dcd512e98d12734cc
Berry  Semexan <bsemexan@gmail.com>
2018-07-06 07:02:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
     erb :'sessions/login'
   end
 
-  post '/sessions' do
+   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
     if @user
       session[:user_id] = @user.id
